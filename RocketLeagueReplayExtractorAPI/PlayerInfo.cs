6ea4856ef6bfe2612093ad56b6754101254223ca
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-04-06 18:34:41 -0400

    Start of Project

---
+++ RocketLeagueReplayExtractorAPI/PlayerInfo.cs
@@ -0,0 +1,152 @@
+﻿using DNARocketLeagueReplayParser.ReplayStructure.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RocketLeagueReplayParserAPI
+{
+    /// <summary>
+    /// Describes the Information of a Player in the Replay
+    /// </summary>
+    public class PlayerInfo
+    {
+        private const string SCORE = "Score";
+        private const string SAVES = "Saves";
+        private const string GOALS = "Goals";
+        private const string ASSISTS = "Assists";
+        private const string NAME = "Name";
+        private const string SHOTS = "Shots";
+        private const string TEAM = "Team";
+
+
+        /// <summary>
+        /// The ID assigned to the Player at the Start of the Match
+        /// </summary>
+        public int PlayerID { get; private set; }
+
+        /// <summary>
+        /// The Name of the Player
+        /// </summary>
+        public string PlayerName { get; private set; }
+
+        /// <summary>
+        /// The Number of Points the Player got
+        /// </summary>
+        public int Score { get; private set; }
+
+        /// <summary>
+        /// The Number of Goals the Player Scored 
+        /// </summary>
+        public int Goals { get; private set; }
+
+        /// <summary>
+        /// The Number of Assists the Player got
+        /// </summary>
+        public int Assists { get; private set; }
+
+        /// <summary>
+        /// The Number of Saves the Player got
+        /// </summary>
+        public int Saves { get; private set; }
+
+        /// <summary>
+        /// The Number of Shots the Player took
+        /// </summary>
+        public int Shots { get; private set; }
+
+        //0 = Blue, 1 = Orange
+        public int Team { get; private set; }
+
+
+        //Eventually support
+        //Clears
+        //Demos
+        //Epic vs Normal Saves
+        //Touches
+        //Boost Usage
+        //ect
+
+        /// <summary>
+        /// Initializes a new Player Info Object
+        /// </summary>
+        /// <param name="playerID"> The ID Assigned to the Player </param>
+        /// <param name="playerName"> The Name of the Player </param>
+        public PlayerInfo(int playerID, string playerName)
+        {
+            PlayerID = playerID;
+            PlayerName = playerName;
+        }
+
+        public PlayerInfo(PropertyDictionary playerStats)
+        {
+
+            PlayerName = (string)playerStats[NAME].Value;
+            Score = (int)playerStats[SCORE].Value;
+            Goals = (int)playerStats[GOALS].Value;
+            Assists = (int)playerStats[ASSISTS].Value;
+            Saves = (int)playerStats[SAVES].Value;
+            Shots = (int)playerStats[SHOTS].Value;
+            Team = (int)playerStats[TEAM].Value;
+
+        }
+
+
+
+
+        /// <summary>
+        /// Sets the Number of Poinrts the Player got
+        /// </summary>
+        /// <param name="score"> The Number of Points/Score the Player got </param>
+        public void SetScore(int score)
+        {
+            Score = score;
+        }
+
+        /// <summary>
+        /// Sets the Number of Goals the Player Scored
+        /// </summary>
+        /// <param name="goals"> The Number of Goals the Player Scored </param>
+        public void SetGoals(int goals)
+        {
+            Goals = goals;
+        }
+
+        /// <summary>
+        /// Sets the Number of Assists the Player got
+        /// </summary>
+        /// <param name="assists"> The Number of Assists </param>
+        public void SetAssists(int assists)
+        {
+            Assists = assists;
+        }
+
+        /// <summary>
+        /// Sets the Number of Saves the Player got
+        /// </summary>
+        /// <param name="saves"> The Number of Saves</param>
+        public void SetSaves(int saves)
+        {
+            Saves = saves;
+        }
+
+        /// <summary>
+        /// Sets the Number of Shots the Player took
+        /// </summary>
+        /// <param name="shots"> The Number of Shots </param>
+        public void SetShots(int shots)
+        {
+            Shots = shots;
+        }
+
+        /// <summary>
+        /// Gets the Information that would displayed on the Scoreboard in the same Format as the Scoreboard
+        /// </summary>
+        /// <returns> Formatted info that would be Displayed on the Scoreboard</returns>
+        public string[] GetScoreboardInfo ()
+        {
+            return [PlayerName, Score.ToString(), Goals.ToString(), Assists.ToString(), Saves.ToString(), Shots.ToString()];
+        }
+    }
+}
---
+++ RocketLeagueReplayExtractorAPI/Replay.cs
@@ -0,0 +1,161 @@
+﻿
+using DNARocketLeagueReplayParser.ReplayStructure;
+using DNARocketLeagueReplayParser.ReplayStructure.Mapping;
+
+
+namespace RocketLeagueReplayParserAPI
+{
+    /// <summary>
+    /// Represents a Rocket League Replay Object
+    /// </summary>
+    public class Replay
+    {
+        public const int BLUE_TEAM = 0;
+        public const int ORANGE_TEAM = 1;
+
+        public const int ZERO_GOALS = 0;
+
+
+        public const string UNNAMED_REPLAY = "Unnamed";
+
+        //Constants
+        private const string CAR = "TAGame.Car_TA";
+        private const string BALL = "TAGame.Ball_TA";
+        private const string PLAYER_NAME = "Engine.PlayerReplicationInfo:PlayerName";
+        private const string PLAYER_REPLICATION_INFO = "Engine.Pawn:PlayerReplicationInfo";
+        private const string VEHICLE = "TAGame.CarComponent_TA:Vehicle";
+        private const string BALL_HIT = "TAGame.Ball_TA:HitTeamNum"; //Or TAGame.GameEvent_Soccar_TA:bBallHasBeenHit
+        private const int RIGID_BODY = 42;
+        private const string PLAYER_STATS = "PlayerStats";
+
+        private const string REPLAY_NAME = "ReplayName";
+
+        /// <summary>
+        /// The Key for the Blue Team Score
+        /// </summary>
+        private const string BLUE_TEAM_SCORE = "Team0Score";
+
+        /// <summary>
+        /// The Key for the Orange Team Score
+        /// </summary>
+        private const string ORANGE_TEAM_SCORE = "Team1Score";
+
+        /// <summary>
+        /// The Replay Info Object
+        /// </summary>
+        private static PsyonixReplay _replayInfo;
+
+        /// <summary>
+        /// The Path to the File Being Parsed
+        /// </summary>
+        private string _pathToFile;
+
+        /// <summary>
+        /// Array of PlayerInfo Objects
+        /// </summary>
+        public PlayerInfo[] Players { get; private set; }
+
+        /// <summary>
+        /// The Blue Teams Number of Goals At the end of the replay
+        /// </summary>
+        public int BlueTeamGoals => TryGetProperty(BLUE_TEAM_SCORE, ZERO_GOALS);
+
+        /// <summary>
+        /// The Orange Teams Number of Goals At the end of the replay
+        /// </summary>
+        public int OrangeTeamGoals => TryGetProperty(ORANGE_TEAM_SCORE, ZERO_GOALS);
+
+        /// <summary>
+        /// The Name of the Replay set by the Player When Saved
+        /// </summary>
+        public string ReplayName => TryGetProperty(REPLAY_NAME, UNNAMED_REPLAY);
+
+        /// <summary>
+        /// Initializes the Replay Object from the given Path
+        /// </summary>
+        /// <param name="path"></param>
+        public Replay(string path)
+        {
+            _pathToFile = path;
+            using (FileStream stream = File.Open(path, FileMode.Open))
+            using (BinaryReader reader = new BinaryReader(stream))
+                _replayInfo = PsyonixReplay.Deserialize(reader);
+
+            ExtractPlayers();
+        }
+
+        /// <summary>
+        /// Tries to the Get the Property from the Replay Info Object <paramref name="defaultValue"/> if it is not found
+        /// </summary>
+        /// <typeparam name="T"> The Return Type of the Object </typeparam>
+        /// <param name="key"> The Key for the Value </param>
+        /// <param name="defaultValue"> The Default Value to Return if value is not Found </param>
+        /// <returns> The Value of the Property being searched, or <paramref name="defaultValue"/> if not found </returns>
+        private T TryGetProperty<T>(string key, T defaultValue)
+        {
+            if (_replayInfo.Properties.TryGetValue(key, out Property value))
+                return (T)value.Value;
+            else
+                return defaultValue;
+        }
+
+        /// <summary>
+        /// Extracts the Players from the Replay Info Object and Formats them into PlayerInfo Objects
+        /// </summary>
+        private void ExtractPlayers()
+        {
+            int playerID = 0;
+
+            ArrayProperty playerStatsArray = (ArrayProperty)_replayInfo.Properties[PLAYER_STATS];
+            List<PropertyDictionary> playerStats = (List<PropertyDictionary>)playerStatsArray.Value;
+
+            Players = new PlayerInfo[playerStats.Count];
+
+            foreach (PropertyDictionary playerStat in playerStats)
+            {
+                Players[playerID] = new PlayerInfo(playerStat);
+                playerID++;
+            }
+        }
+
+        /// <summary>
+        /// Gets the Number of Saves for a Team
+        /// </summary>
+        /// <param name="blueTeam"> Flag indicating if the Team to get is Blue </param>
+        /// <returns> The Number of Saves for the Team </returns>
+        public int GetTeamSaves(bool blueTeam)
+        {
+            int teamID = blueTeam ? BLUE_TEAM : ORANGE_TEAM;
+            int saves = 0;
+
+            foreach (PlayerInfo player in Players)
+            {
+                if (player.Team == teamID)
+                    saves += player.Saves;
+            }
+
+            return saves;
+        }
+
+        /// <summary>
+        /// Renames the Replay and Saves it to the given Path
+        /// </summary>
+        /// <param name="renamedName"> The new Name of the File, if left empty it defaults to the Name of the Replay </param>
+        /// <param name="savePath"> The New Save Path of the Replay, if left Empty it saves in the Same Directory </param>
+        /// <exception cref="DirectoryNotFoundException"> Error thrown if the new Directory doesn't exist </exception>
+        public void RenameAndSave (string? renamedName = null, string? savePath = null)
+        {
+            if (string.IsNullOrEmpty(savePath))
+                savePath = Path.GetDirectoryName(_pathToFile);
+
+            if (string.IsNullOrEmpty(renamedName))
+                renamedName = ReplayName;
+
+            if (Directory.Exists(savePath) == false)
+                throw new DirectoryNotFoundException("The Directory to Save the Replay to does not exist");
+
+            string newPath = Path.Combine(savePath, renamedName + ".replay");
+            File.WriteAllText(newPath, File.ReadAllText(_pathToFile));
+        }
+    }
+}
