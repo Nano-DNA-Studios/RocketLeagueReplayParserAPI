024ba01e2865708252a55760b14e3c945fb606df
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-04-12 22:38:48 -0400

    So Far

---
+++ RocketLeagueReplayExtractorAPI/BallTouch.cs
@@ -0,0 +1,15 @@
+﻿using DNARocketLeagueReplayParser.ReplayStructure.UnrealEngineObjects;
+
+namespace RocketLeagueReplayParserAPI
+{
+    public class BallTouch
+    {
+        public int FrameNumber { get; set; }
+
+        public float Time { get; set; }
+
+        public uint ActorID { get; set; }
+
+        public RigidBodyState RigidBody { get; set; }
+    }
+}
--- RocketLeagueReplayExtractorAPI/TouchCalculator.cs
+++ RocketLeagueReplayExtractorAPI/TouchCalculator.cs
@@ -8,6 +8,21 @@ namespace RocketLeagueReplayParserAPI
 {
     internal class TouchCalculator
     {
+        public List<GameObjectState> BallPosition { get; set; }
+
+        public Dictionary<string, List<GameObjectState>> CarPositions { get; set; }
+
+        private Dictionary<int, List<GameObjectState>> _carPositionsByFrame { get; set; }
+
+
+        public TouchCalculator(List<GameObjectState> ballPosition, Dictionary<string, List<GameObjectState>> carPositions)
+        {
+            BallPosition = ballPosition;
+            CarPositions = carPositions;
+        }
+
+
+
 
 
 
